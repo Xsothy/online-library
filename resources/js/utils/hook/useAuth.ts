0d581ddc8e42835bb28ebc5f9e8c0b79c06ac0f6
sothylor <panha.saloeun@gmail.com>
2024-08-30 20:03:38 -0700

    update useAuth

---
+++ resources/js/utils/hook/useAuth.ts
@@ -0,0 +1,12 @@
+import {usePage} from "@/utils/hook/usePage";
+import {Effect} from "effect";
+import {UnAuthorizedError} from "@/errors";
+
+
+export const useAuth =  usePage.pipe(
+    Effect.map(page => page.props.auth),
+    Effect.flatMap(auth => {
+        if (!auth) return Effect.fail(new UnAuthorizedError);
+        return Effect.succeed(auth);
+    })
+)
