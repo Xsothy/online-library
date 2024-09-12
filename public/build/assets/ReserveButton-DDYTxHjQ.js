6562f6612a38d0f16f24c53bb9b9b240dd3a9b29
sothylor <panha.saloeun@gmail.com>
2024-09-11 22:21:29 -0700

    build b3

---
+++ public/build/assets/ReserveButton-DDYTxHjQ.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CXkTVRn7.js";import{B as s}from"./button-CI8YzJOI.js";import"./utils-DnNDQBbQ.js";function m({bookId:i,onReserve:t,isReserving:e,setIsReserving:o}){const r=()=>{o(!0),t()};return n.jsx(s,{onClick:r,disabled:e,children:e?"Reserving...":"Reserve"})}export{m as default};
